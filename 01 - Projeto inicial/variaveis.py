94dca4310f86fe0bbfef89a473c11416709606e3
Osni Filipo <osnifilipo@gmail.com>
2023-06-12 21:00:36 -0300

    Update variaveis.py

--- 01 - Projeto inicial/variaveis.py
+++ 01 - Projeto inicial/variaveis.py
@@ -1,22 +1,39 @@
 texto = 'Olá Mundo!'
+# A variável 'texto' armazena uma string com o valor 'Olá Mundo!'
 print(type(texto))
+# Imprime o tipo de dado da variável 'texto' (str)
 print(texto)
+# Imprime o valor da variável 'texto' (Olá Mundo!)
 
 inteiro = 24
+# A variável 'inteiro' armazena um número inteiro com o valor 24
 print(type(inteiro))
-print("Inteiro:",inteiro)
+# Imprime o tipo de dado da variável 'inteiro' (int)
+print("Inteiro:", inteiro)
+# Imprime a mensagem "Inteiro:" seguida do valor da variável 'inteiro'
 
 decimal = 24.6
+# A variável 'decimal' armazena um número decimal (ponto flutuante) com o valor 24.6
 print(type(decimal))
-print("Float:",decimal)
+# Imprime o tipo de dado da variável 'decimal' (float)
+print("Float:", decimal)
+# Imprime a mensagem "Float:" seguida do valor da variável 'decimal'
 
 complexo = 24 + 8j
+# A variável 'complexo' armazena um número complexo com a parte real 24 e a parte imaginária 8j
 print(type(complexo))
-print("Complexo:",complexo)
+# Imprime o tipo de dado da variável 'complexo' (complex)
+print("Complexo:", complexo)
+# Imprime a mensagem "Complexo:" seguida do valor da variável 'complexo'
 
 booleano = True
+# A variável 'booleano' armazena um valor booleano True (verdadeiro)
 print(type(booleano))
+# Imprime o tipo de dado da variável 'booleano' (bool)
 print("Booleano:", booleano)
+# Imprime a mensagem "Booleano:" seguida do valor da variável 'booleano'
 
-print(inteiro,decimal, sep = '-', end = '--')
+print(inteiro, decimal, sep='-', end='--')
+# Imprime os valores das variáveis 'inteiro' e 'decimal', separados por '-' e com '--' no final
 print(complexo)
+# Imprime o valor da variável 'complexo'
